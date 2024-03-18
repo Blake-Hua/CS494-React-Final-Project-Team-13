fed09a2aa95ecb8004e058bc7db8bab527a48727
Justin <fernbaughj@gmail.com>
2024-03-18 16:34:03 -0700

    Change navigation to asian and add cardsg

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // import { Home, People, Planets, Films, Root, PersonDetails, PlanetDetails, FilmDetails, ErrorPage} from './Routes'
-import { Home } from './pages/Home'
-import { People, PersonDetails } from './pages/People'
-import { Planets, PlanetDetails} from './pages/Planets'
-import { Films, FilmDetails} from './pages/Films'
-import { Root } from './Root'
-import { ErrorPage } from './pages/Error'
+import { Home } from "./pages/Home";
+import { Asian, People, PersonDetails } from "./pages/Asian";
+import { Planets, PlanetDetails } from "./pages/Planets";
+import { Films, FilmDetails } from "./pages/Films";
+import { Root } from "./Root";
+import { ErrorPage } from "./pages/Error";
 
-import './index.css'
+import "./index.css";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <Root><ErrorPage /></Root>,
-        children: [
-            { index: true, element: <Home/>},
-            {    
-                path: "/people", 
-                element: <People/>,
-                children: [
-                    { path: ":personDetails", element: <PersonDetails/> }
-                ]
-            },
-            { 
-                path: "/planets", 
-                element: <Planets/>,
-                children: [
-                    { path: ":planetDetails", element: <PlanetDetails/> }
-                ]            
-            },
-            {
-                path: "/films", 
-                element: <Films/>,
-                children: [
-                    { path: ":filmDetails", element: <FilmDetails/>}
-                ]
-            }
-        ]
-    },
+	{
+		path: "/",
+		element: <Root />,
+		errorElement: (
+			<Root>
+				<ErrorPage />
+			</Root>
+		),
+		children: [
+			{ index: true, element: <Home /> },
+			{
+				path: "/asian",
+				element: <Asian />,
+				children: [{ path: ":personDetails", element: <PersonDetails /> }],
+			},
+			{
+				path: "/planets",
+				element: <Planets />,
+				children: [{ path: ":planetDetails", element: <PlanetDetails /> }],
+			},
+			{
+				path: "/films",
+				element: <Films />,
+				children: [{ path: ":filmDetails", element: <FilmDetails /> }],
+			},
+		],
+	},
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
--- src/pages/Asian.jsx
+++ src/pages/Asian.jsx
@@ -1,58 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import { Link, NavLink, Outlet, useParams} from "react-router-dom"
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from "react";
+import { Link, NavLink, Outlet, useParams } from "react-router-dom";
 
-import peopleData from "../data/people.json";
+// import peopleData from "../data/people.json";
+import asianRecipes from "../data/response.json";
 
 export function People() {
-    // const [people, setPeople] = useState([]);
-  
-    // useEffect(() => {
-    //   setPeople(Object.values(peopleData));
-    // }, []);
-  
-    // return (
-    //   <> 
-    //     <Outlet />
-    //     <ul className="sidebar">
-    //         {people.map((person) => (
-    //             <li key={person.name}>
-    //                 <NavLink to={person.url}>{person.name}</NavLink>
-    //             </li>
-    //         ))}
-    //     </ul>
-    //   </>
-    // );
+	// const [people, setPeople] = useState([]);
+	// useEffect(() => {
+	//   setPeople(Object.values(peopleData));
+	// }, []);
+	// return (
+	//   <>
+	//     <Outlet />
+	//     <ul className="sidebar">
+	//         {people.map((person) => (
+	//             <li key={person.name}>
+	//                 <NavLink to={person.url}>{person.name}</NavLink>
+	//             </li>
+	//         ))}
+	//     </ul>
+	//   </>
+	// );
 }
 
 export function PersonDetails() {
-    // const params = useParams();
+	// const params = useParams();
+	// const personDetails = peopleData[params.personDetails];
+	// return (
+	//   <>
+	//     <div className="mainInfo">
+	//         <h2>{personDetails.name}</h2>
+	//         <p>Height: {personDetails.height}</p>
+	//         <p>Mass: {personDetails.mass}</p>
+	//         <p>Hair Color: {personDetails.hair_color}</p>
+	//         <p>Skin Color: {personDetails.skin_color}</p>
+	//         <p>Eye Color: {personDetails.eye_color}</p>
+	//         <p>Birth Year: {personDetails.birth_year}</p>
+	//         <p>Gender: {personDetails.gender}</p>
+	//         <p>
+	//             Homeworld: {" "}
+	//             <Link to={personDetails.homeworld}>{personDetails.homeworld}</Link>
+	//         </p>
+	//         <p>Films:</p>
+	//         <ul>
+	//         {personDetails.films.map((filmUrl) => (
+	//             <li key={filmUrl}>
+	//                 <Link to={filmUrl}>{filmUrl}</Link>
+	//             </li>
+	//         ))}
+	//         </ul>
+	//     </div>
+	//   </>
+	// )
+}
 
-    // const personDetails = peopleData[params.personDetails];
-  
-    // return (
-    //   <>
-    //     <div className="mainInfo">
-    //         <h2>{personDetails.name}</h2>
-    //         <p>Height: {personDetails.height}</p>
-    //         <p>Mass: {personDetails.mass}</p>
-    //         <p>Hair Color: {personDetails.hair_color}</p>
-    //         <p>Skin Color: {personDetails.skin_color}</p>
-    //         <p>Eye Color: {personDetails.eye_color}</p>
-    //         <p>Birth Year: {personDetails.birth_year}</p>
-    //         <p>Gender: {personDetails.gender}</p>
-    //         <p>
-    //             Homeworld: {" "}
-    //             <Link to={personDetails.homeworld}>{personDetails.homeworld}</Link>
-    //         </p>
-    //         <p>Films:</p>
-    //         <ul>
-    //         {personDetails.films.map((filmUrl) => (
-    //             <li key={filmUrl}>
-    //                 <Link to={filmUrl}>{filmUrl}</Link>
-    //             </li>
-    //         ))}
-    //         </ul>
-    //     </div>
-    //   </>
-    // )
+// Here we want to take the sample query from asianRecipes and display the recipes inside results as cards
+// We want to display the title, image, readyInMinutes, and the sourceUrl
+// We want to use the image as a link to the sourceUrl
+// We want to display the title as a header
+// This might take multiple components/functions to accomplish such as having a RecipeCard component
+export function Asian() {
+	const [recipes, setRecipes] = useState([]);
+	useEffect(() => {
+		setRecipes(asianRecipes.results);
+	}, []);
+	return (
+		<>
+			<Outlet />
+			<div className='recipeContainer'>
+				{recipes.map((recipe) => (
+					<RecipeCard key={recipe.id} recipe={recipe} />
+				))}
+			</div>
+		</>
+	);
+}
+
+function RecipeCard({ recipe }) {
+	return (
+		<div className='recipeCard'>
+			<a href={recipe.sourceUrl}>
+				<img src={recipe.image} alt={recipe.title} />
+				<h2>{recipe.title}</h2>
+				<p>Ready in {recipe.readyInMinutes} minutes</p>
+			</a>
+		</div>
+	);
 }
