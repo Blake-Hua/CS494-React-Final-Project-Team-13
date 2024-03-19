4f59860a032e4c385f2e322952037b06bb752d3c
Justin <fernbaughj@gmail.com>
2024-03-18 17:18:46 -0700

    Implement Asian cards

--- src/pages/Asian.jsx
+++ src/pages/Asian.jsx
@@ -1,73 +1,46 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
 import { Link, NavLink, Outlet, useParams } from "react-router-dom";
+import axios from "axios";
 
-// import peopleData from "../data/people.json";
-import asianRecipes from "../data/response.json";
+// import asianRecipes from "../data/response.json";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
-export function People() {
-	// const [people, setPeople] = useState([]);
-	// useEffect(() => {
-	//   setPeople(Object.values(peopleData));
-	// }, []);
-	// return (
-	//   <>
-	//     <Outlet />
-	//     <ul className="sidebar">
-	//         {people.map((person) => (
-	//             <li key={person.name}>
-	//                 <NavLink to={person.url}>{person.name}</NavLink>
-	//             </li>
-	//         ))}
-	//     </ul>
-	//   </>
-	// );
-}
+export function People() {}
 
-export function PersonDetails() {
-	// const params = useParams();
-	// const personDetails = peopleData[params.personDetails];
-	// return (
-	//   <>
-	//     <div className="mainInfo">
-	//         <h2>{personDetails.name}</h2>
-	//         <p>Height: {personDetails.height}</p>
-	//         <p>Mass: {personDetails.mass}</p>
-	//         <p>Hair Color: {personDetails.hair_color}</p>
-	//         <p>Skin Color: {personDetails.skin_color}</p>
-	//         <p>Eye Color: {personDetails.eye_color}</p>
-	//         <p>Birth Year: {personDetails.birth_year}</p>
-	//         <p>Gender: {personDetails.gender}</p>
-	//         <p>
-	//             Homeworld: {" "}
-	//             <Link to={personDetails.homeworld}>{personDetails.homeworld}</Link>
-	//         </p>
-	//         <p>Films:</p>
-	//         <ul>
-	//         {personDetails.films.map((filmUrl) => (
-	//             <li key={filmUrl}>
-	//                 <Link to={filmUrl}>{filmUrl}</Link>
-	//             </li>
-	//         ))}
-	//         </ul>
-	//     </div>
-	//   </>
-	// )
-}
+export function PersonDetails() {}
 
 // Here we want to take the sample query from asianRecipes and display the recipes inside results as cards
 // We want to display the title, image, readyInMinutes, and the sourceUrl
 // We want to use the image as a link to the sourceUrl
 // We want to display the title as a header
 // This might take multiple components/functions to accomplish such as having a RecipeCard component
+
+// turn this url into an axois GET request https://api.spoonacular.com/recipes/complexSearch?apiKey=c062a555de194d4d824cac22bc0a4d2e&query=asian&number=5
+// make sure to break the query params into an object and use the object in the get request
 export function Asian() {
 	const [recipes, setRecipes] = useState([]);
+
+	async function fetchData() {
+		const response = await axios.get(
+			"https://api.spoonacular.com/recipes/complexSearch",
+			{
+				params: {
+					apiKey: API_KEY,
+					query: "asian",
+					number: 10,
+				},
+			}
+		);
+		setRecipes(response.data.results);
+	}
+
 	useEffect(() => {
-		setRecipes(asianRecipes.results);
+		fetchData();
 	}, []);
+
 	return (
 		<>
-			<Outlet />
 			<div className='recipeCardContainer'>
 				{recipes.map((recipe) => (
 					<RecipeCard key={recipe.id} recipe={recipe} />
